5384f2ba3cd439c67016393bea32f38351805493
Kasun Balasooriya <kmbkck1@gmail.com>
2021-09-25 19:39:03 +0530

    implementing task model and new task items

--- src/main/frontend/src/app/tasks/tasks-list/tasks-list.component.ts
+++ src/main/frontend/src/app/tasks/tasks-list/tasks-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Task} from "../task.model";
 
 @Component({
   selector: 'app-tasks-list',
@@ -7,9 +8,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TasksListComponent implements OnInit {
 
+    tasks: Task[] =[];
+
   constructor() { }
 
   ngOnInit(): void {
+      this.tasks.push(new Task(1, "Task 1", true,"27/01/2021"))
+      this.tasks.push(new Task(2, "Task 2", false,"27/01/2021"))
+      this.tasks.push(new Task(3, "Task 3", false,"27/01/2021"))
+      this.tasks.push(new Task(4, "Task 4", false,"27/01/2021"))
   }
 
+    onTaskChange($event: Event, task: Task) {
+        console.log("this task changed "+task.name)
+    }
+
+    getDueDateLabel(task: Task) {
+        return task.completed ? 'bg-success' : 'bg-primary';
+    }
 }
